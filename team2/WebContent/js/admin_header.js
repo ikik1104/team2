daffacd5d7b7bfacb79b42aa6582a013da8b884b
ikik1104 <42055126+ikik1104@users.noreply.github.com>
2020-03-31 17:15:46 +0900

    20.03.31 프로젝트

---
+++ team2/WebContent/js/admin_header.js
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+
+$(document).ready(function() {
+//	$(".sub_list").hide();
+	
+	$("#student").children("a").eq(0).click(function() {
+		$("#stu_list").slideToggle();
+		
+		if($("#s_icon").attr("class")=="icon_minus"){
+			$("#s_icon").attr("class","icon_plus");
+		}else{
+			$("#s_icon").attr("class","icon_minus");
+		}
+	});
+	
+	
+	$("#teacher").children("a").eq(0).click(function() {
+		$("#tes_list").slideToggle();
+		if($("#t_icon").attr("class")=="icon_minus2"){
+			$("#t_icon").attr("class","icon_plus2");
+		}else{
+			$("#t_icon").attr("class","icon_minus2");
+		}
+	});
+	$("#menu ul li").hover(function() {
+		$(this).children("ul").css("visibility","visible");
+	}, function() {
+		$(this).children("ul").css("visibility","hidden");
+	})
+	
+	
+	$(".sub_list li").hover(function() {
+		$(this).css("background-color","#dddddd");
+	}, function() {
+		$(this).css("background-color","#efefef");
+	})
+	
+	$(".user ul li").hover(function() {
+		$(this).css("font-weight","bold");
+	}, function() {
+		$(this).css("font-weight","normal");
+	})
+	
+	
+//	
+//	$("#menu ul li").hover(function() {
+//	});
+//	$("#menu ul li").mouseleave(function() {
+//	});
+//	
+////	$("#menu ul li").hover(function() {
+////		$(this).find("ul").slideToggle("fast");
+////	});
+//	
+//	$(".sub_list li").hover(
+//		$(this).css("background-color","#dddddd");
+//	);
+//	
+});
+
